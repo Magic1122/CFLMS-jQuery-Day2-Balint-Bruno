85777cc6bcaa13b6f2d480cb13f6f73b88d381ef
Balint Bakos <balint.bb@gmail.com>
2020-06-10 11:20:40 +0200

    adds interm

--- Intermediate/myscript.js
+++ Intermediate/myscript.js
@@ -8,12 +8,13 @@ $(document).ready(function() {
 
     for (let i = 0; i < persons.length; i++) {
 
-        let person = `<div class="person_wrapper">
+        let person = `<div class="person_wrapper" id="person_wrapper${i + 1}">
 	<img src="${persons[i].image}">
-		<div class="person_name">
+		<div class="person_name" id='person_name${i + 1}'>
 			<p>${persons[i].name}</p>
 			<p class="person_relation">${persons[i].relation}</p>
 		</div>
+		<div id="hobbies${i + 1}"></div>
 	</div>`;
         $('#wrapper').append(person);
     }
@@ -21,26 +22,26 @@ $(document).ready(function() {
 
 
 
-    let button = `<button class ="button" type="button">Famylize</button>`;
-    $('body').append(button);
+
+for (let i = 1; i <= persons.length; i++) {
+	$(`#person_wrapper${i}`).on('click', (e) => {
+		$(`#hobbies${i}`).empty()
+		console.log(e)
+		let hobbiesNode
+		persons.map((person, index) => {
+			 hobbiesNode = person.hobbies.map((hobbie) => {
+				return `<p>${hobbie}</p>`
+			})
+		})
+		$(`#hobbies${i}`).append(hobbiesNode)
+	})
+}
 
 
 
-
-
-    $('.button').click(familize);
-
-    function familize() {
-        for (let i = 0; i < persons.length; i++) {
-            if (!persons[i].family) {
-                $(`.person_wrapper:nth-child(${i+1})`).addClass('nonFamily');
-            } else {
-                $(`.person_wrapper:nth-child(${i+1})`).addClass('family');
-            }
-        }
-    }
-
-
-
+$('.person_wrapper').on('dblclick', (e) => {
+	console.log(e.target)
+	$(e.target).remove()
+})
 
 });
