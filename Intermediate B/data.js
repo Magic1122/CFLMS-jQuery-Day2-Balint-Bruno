829afde1eec1e29ef9cf815e2513d05bdc7a38a0
brunomairey <bruno.mairey@yahoo.fr>
2020-06-10 15:05:25 +0200

    redo exo2

---
+++ Intermediate B/data.js
@@ -0,0 +1,106 @@
+const simpsons = [{
+        "name": "Homer",
+        "relation": "Father",
+        "family": true,
+        "image": "./img/homer.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Marge",
+        "relation": "Mother",
+        "family": true,
+        "image": "./img/marge.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Bart",
+        "relation": "Son",
+        "family": true,
+        "image": "./img/bart.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "lada"
+    },
+    {
+        "name": "Lisa",
+        "relation": "Daughter",
+        "family": true,
+        "image": "./img/lisa.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Maggie",
+        "relation": "Daughter",
+        "family": true,
+        "image": "./img/maggie.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Flanders",
+        "relation": "Neighbour",
+        "family": false,
+        "image": "./img/flanders.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Mr. Burns",
+        "relation": "Boss",
+        "family": false,
+        "image": "./img/burns.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Smithers",
+        "relation": "Victim",
+        "family": false,
+        "image": "./img/smithers.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Apu",
+        "relation": "Friend",
+        "family": false,
+        "image": "./img/apu.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Comic book guy",
+        "relation": "Vendor",
+        "family": false,
+        "image": "./img/comic_book_guy.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Itchy",
+        "relation": "TV Star",
+        "family": false,
+        "image": "./img/itchy.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Krusty",
+        "relation": "TV Star",
+        "family": false,
+        "image": "./img/krusty.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    },
+    {
+        "name": "Ralph",
+        "relation": "Friend",
+        "family": false,
+        "image": "./img/ralph.png",
+        "hobbies": ["reading", "climbing", "biking"],
+        "car": "tesla"
+    }
+
+]
---
+++ Intermediate B/myscriptb2.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    $('*').css({ 'margin': '0', 'padding': '0' });
+
+    const persons = (simpsons);
+    console.log(persons)
+
+
+    for (let i = 0; i < persons.length; i++) {
+
+        let person = `<div class="person_wrapper" id="person_wrapper${i + 1}">
+	<img src="${persons[i].image}">
+		<div class="person_name" id='person_name${i + 1}'>
+			<p>${persons[i].name}</p>
+			<p class="person_relation">${persons[i].relation}</p>
+		</div>
+		<div id="hobbies${i + 1}"></div>
+		<div id="cars${i + 1}"></div>
+	</div>`;
+        $('#wrapper').append(person);
+    }
+
+
+
+
+
+    for (let i = 1; i <= persons.length; i++) {
+        $(`#person_wrapper${i}`).on('click', (e) => {
+            $(`#hobbies${i}`).empty()
+            $(`#cars${i}`).empty()
+            console.log(e)
+
+            let hobbiesNode
+            let carNode
+            persons.map((person) => {
+                carNode = person.car
+                hobbiesNode = person.hobbies.map((hobbie) => {
+                    return `<p>${hobbie}</p>`
+                })
+            })
+            $(`#hobbies${i}`).append(hobbiesNode)
+            $(`#cars${i}`).append(carNode)
+
+        })
+    }
+
+
+
+    $('.person_wrapper').on('dblclick', (e) => {
+        console.log(e.target)
+        $(e.target).remove()
+    })
+
+});
