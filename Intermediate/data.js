889287c0885d86889f12065ec2a541f779bf9601
brunomairey <bruno.mairey@yahoo.fr>
2020-06-10 10:36:42 +0200

    int

--- Intermediate/data.js
+++ Intermediate/data.js
@@ -3,79 +3,105 @@ const simpsons = [
             "name": "Homer",
             "relation": "Father",
             "family": true,
-            "image": "./img/homer.png"
+            "image": "./img/homer.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Marge",
             "relation": "Mother",
             "family": true,
-            "image": "./img/marge.png"
+            "image": "./img/marge.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Bart",
             "relation": "Son",
             "family": true,
-            "image": "./img/bart.png"
+            "image": "./img/bart.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Lisa",
             "relation": "Daughter",
             "family": true,
-            "image": "./img/lisa.png"
+            "image": "./img/lisa.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Maggie",
             "relation": "Daughter",
             "family": true,
-            "image": "./img/maggie.png"
+            "image": "./img/maggie.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Flanders",
             "relation": "Neighbour",
             "family": false,
-            "image": "./img/flanders.png"
+            "image": "./img/flanders.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Mr. Burns",
             "relation": "Boss",
             "family": false,
-            "image": "./img/burns.png"
+            "image": "./img/burns.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Smithers",
             "relation": "Victim",
             "family": false,
-            "image": "./img/smithers.png"
+            "image": "./img/smithers.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Apu",
             "relation": "Friend",
             "family": false,
-            "image": "./img/apu.png"
+            "image": "./img/apu.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Comic book guy",
             "relation": "Vendor",
             "family": false,
-            "image": "./img/comic_book_guy.png"
+            "image": "./img/comic_book_guy.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Itchy",
             "relation": "TV Star",
             "family": false,
-            "image": "./img/itchy.png"
+            "image": "./img/itchy.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Krusty",
             "relation": "TV Star",
             "family": false,
-            "image": "./img/krusty.png"
+            "image": "./img/krusty.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         },
         {
             "name": "Ralph",
             "relation": "Friend",
             "family": false,
-            "image": "./img/ralph.png"
+            "image": "./img/ralph.png",
+            "hobbies": ["reading","climbing","biking"],
+            "car" : "tesla"
         }
     
 ]
--- Intermediate/myscript.js
+++ Intermediate/myscript.js
@@ -1,41 +1,44 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-$('*').css({'margin':'0', 'padding':'0'});
+    $('*').css({ 'margin': '0', 'padding': '0' });
 
-const persons = JSON.parse(simpsons);
-console.log(persons)
+    const persons = (simpsons);
+    console.log(persons)
 
 
-for (let i = 0; i < persons.length; i++){
+    for (let i = 0; i < persons.length; i++) {
 
-	let person = `<div class="person_wrapper">
+        let person = `<div class="person_wrapper">
 	<img src="${persons[i].image}">
 		<div class="person_name">
 			<p>${persons[i].name}</p>
 			<p class="person_relation">${persons[i].relation}</p>
 		</div>
 	</div>`;
-	$('#wrapper').append(person);
-}
-
-let button = `<button class ="button" type="button">Famylize</button>`;
-$('body').append(button);
+        $('#wrapper').append(person);
+    }
 
 
 
 
+    let button = `<button class ="button" type="button">Famylize</button>`;
+    $('body').append(button);
 
-$('.button').click(familize);
-function familize(){
-	for (let i = 0; i < persons.length; i++){
-		if(!persons[i].family){
-			$(`.person_wrapper:nth-child(${i+1})`).addClass('nonFamily');
-		}
-		else{
-			$(`.person_wrapper:nth-child(${i+1})`).addClass('family');
-		}		 
-	}	
-}
+
+
+
+
+    $('.button').click(familize);
+
+    function familize() {
+        for (let i = 0; i < persons.length; i++) {
+            if (!persons[i].family) {
+                $(`.person_wrapper:nth-child(${i+1})`).addClass('nonFamily');
+            } else {
+                $(`.person_wrapper:nth-child(${i+1})`).addClass('family');
+            }
+        }
+    }
 
 
 
